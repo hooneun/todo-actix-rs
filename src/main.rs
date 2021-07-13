66e8e6b219408521034856f1ad9dea9a882cd19e
kimhoon <kimhoon0316@gmail.com>
2021-07-13 16:41:26 +0900

    database setting, get_todos handler add

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
+mod config;
+mod db;
+mod handlers;
 mod models;
 
-use actix_web::{web, App, HttpServer, Responder};
-use models::Status;
+use crate::handlers::*;
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
 use std::io;
-
-async fn status() -> impl Responder {
-    web::HttpResponse::Ok().json(Status {
-        status: "UP".to_string(),
-    })
-}
+use tokio_postgres::NoTls;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
-    println!("Server Start 127.0.0.1:8080");
+    dotenv().ok();
 
-    HttpServer::new(|| App::new().route("/", web::get().to(status)))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    let config = config::Config::from_env().unwrap();
+
+    let pool = config.pg.create_pool(NoTls).unwrap();
+
+    println!(
+        "Server Start http://{}:{}",
+        config.server.host, config.server.port
+    );
+
+    HttpServer::new(move || {
+        App::new()
+            .data(pool.clone())
+            .route("/", web::get().to(status))
+            .route("/todos{_:/?}", web::get().to(get_todos))
+    })
+    .bind(format!("{}:{}", config.server.host, config.server.port))?
+    .run()
+    .await
 }
