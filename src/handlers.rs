5e3eda49999b4f629427fb4e0455137ba777bce1
kimhoon <kimhoon0316@gmail.com>
2021-07-13 18:06:18 +0900

    create_todo, check_item add

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,7 +1,8 @@
 use crate::db;
-use crate::models::Status;
+use crate::models::{CreateTodoList, ResultResponse, Status};
 use actix_web::{web, HttpResponse, Responder};
 use deadpool_postgres::{Client, Pool};
+use std::io::ErrorKind::Other;
 
 pub async fn status() -> impl Responder {
     web::HttpResponse::Ok().json(Status {
@@ -19,6 +20,56 @@ pub async fn get_todos(db_pool: web::Data<Pool>) -> impl Responder {
 
     match result {
         Ok(todos) => HttpResponse::Ok().json(todos),
+        Err(_) => HttpResponse::InternalServerError().into(),
+    }
+}
+
+pub async fn get_items(db_pool: web::Data<Pool>, path: web::Path<(i32,)>) -> impl Responder {
+    let client: Client = db_pool
+        .get()
+        .await
+        .expect("Error connecting to the database");
+
+    let result = db::get_items(&client, path.into_inner().0).await;
+
+    match result {
+        Ok(items) => HttpResponse::Ok().json(items),
+        Err(_) => HttpResponse::InternalServerError().into(),
+    }
+}
+
+pub async fn create_todo(
+    db_pool: web::Data<Pool>,
+    json: web::Json<CreateTodoList>,
+) -> impl Responder {
+    let client: Client = db_pool
+        .get()
+        .await
+        .expect("Error connecting to the database");
+
+    let result = db::create_todo(&client, json.title.clone()).await;
+
+    match result {
+        Ok(todos) => HttpResponse::Ok().json(todos),
+        Err(_) => HttpResponse::InternalServerError().into(),
+    }
+}
+
+pub async fn check_item(db_pool: web::Data<Pool>, path: web::Path<(i32, i32)>) -> impl Responder {
+    let client: Client = db_pool
+        .get()
+        .await
+        .expect("Error connecting to the database");
+
+    let path = path.into_inner();
+
+    let result = db::check_item(&client, path.0, path.1).await;
+
+    match result {
+        Ok(()) => HttpResponse::Ok().json(ResultResponse { success: true }),
+        Err(ref e) if e.kind() == Other => {
+            HttpResponse::Ok().json(ResultResponse { success: false })
+        }
         Err(_) => HttpResponse::InternalServerError().into(),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,12 @@ async fn main() -> io::Result<()> {
             .data(pool.clone())
             .route("/", web::get().to(status))
             .route("/todos{_:/?}", web::get().to(get_todos))
+            .route("/todos{_:/?}", web::post().to(create_todo))
+            .route("/todos/{list_id}/items", web::get().to(get_items))
+            .route(
+                "/todos/{list_id}/items/{item_id}{_:/?}",
+                web::put().to(check_item),
+            )
     })
     .bind(format!("{}:{}", config.server.host, config.server.port))?
     .run()
