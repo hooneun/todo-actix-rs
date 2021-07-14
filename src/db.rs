197f8838fc7dce477be12799c936cbaa7df396f2
kimhoon <kimhoon0316@gmail.com>
2021-07-14 12:28:23 +0900

    error log add

--- src/db.rs
+++ src/db.rs
@@ -1,18 +1,21 @@
-use crate::models::{TodoItem, TodoList};
+use crate::{
+    errors::{AppError, AppErrorType},
+    models::{TodoItem, TodoList},
+};
 use deadpool_postgres::Client;
 use std::io;
 use tokio_pg_mapper::FromTokioPostgresRow;
 
-pub async fn get_todos(client: &Client) -> Result<Vec<TodoList>, io::Error> {
+pub async fn get_todos(client: &Client) -> Result<Vec<TodoList>, AppError> {
     let statement = client
         .prepare("SELECT * FROM todo_list ORDER BY id DESC LIMIT 10")
         .await
-        .unwrap();
+        .map_err(AppError::db_error)?;
 
     let todos = client
         .query(&statement, &[])
         .await
-        .expect("Error getting todo lists")
+        .map_err(AppError::db_error)?
         .iter()
         .map(|row| TodoList::from_row_ref(row).unwrap())
         .collect::<Vec<TodoList>>();
@@ -20,16 +23,16 @@ pub async fn get_todos(client: &Client) -> Result<Vec<TodoList>, io::Error> {
     Ok(todos)
 }
 
-pub async fn get_items(client: &Client, list_id: i32) -> Result<Vec<TodoItem>, io::Error> {
+pub async fn get_items(client: &Client, list_id: i32) -> Result<Vec<TodoItem>, AppError> {
     let statement = client
         .prepare("SELECT * FROM todo_item WHERE list_id = $1 ORDER BY ID")
         .await
-        .unwrap();
+        .map_err(AppError::db_error)?;
 
     let items = client
         .query(&statement, &[&list_id])
         .await
-        .expect("Error getting todo lists")
+        .map_err(AppError::db_error)?
         .iter()
         .map(|row| TodoItem::from_row_ref(row).unwrap())
         .collect::<Vec<TodoItem>>();
@@ -37,39 +40,40 @@ pub async fn get_items(client: &Client, list_id: i32) -> Result<Vec<TodoItem>, i
     Ok(items)
 }
 
-pub async fn create_todo(client: &Client, title: String) -> Result<TodoList, io::Error> {
+pub async fn create_todo(client: &Client, title: String) -> Result<TodoList, AppError> {
     let statement = client
         .prepare("INSERT INTO todo_list (title) VALUES ($1) RETURNING id, title")
         .await
-        .unwrap();
+        .map_err(AppError::db_error)?;
 
     client
         .query(&statement, &[&title])
         .await
-        .expect("Error creating todo list")
+        .map_err(AppError::db_error)?
         .iter()
         .map(|row| TodoList::from_row_ref(row).unwrap())
         .collect::<Vec<TodoList>>()
         .pop()
-        .ok_or(io::Error::new(
-            io::ErrorKind::Other,
-            "Error creating todo list",
-        ))
+        .ok_or(AppError {
+            message: Some("Error creating Todo list".to_string()),
+            cause: Some("Unknown error".to_string()),
+            error_type: AppErrorType::DbError,
+        })
 }
 
-pub async fn check_item(client: &Client, list_id: i32, item_id: i32) -> Result<(), io::Error> {
+pub async fn check_item(client: &Client, list_id: i32, item_id: i32) -> Result<bool, AppError> {
     let statement = client
         .prepare("UPDATE todo_item SET checked = true where list_id = $1 and id = $2 and checked = false")
         .await
-        .unwrap();
+        .map_err(AppError::db_error)?;
 
     let result = client
         .execute(&statement, &[&list_id, &item_id])
         .await
-        .expect("Error checking todo item");
+        .map_err(AppError::db_error)?;
 
     match result {
-        ref updated if *updated == 1 => Ok(()),
-        _ => Err(io::Error::new(io::ErrorKind::Other, "Failed to check list")),
+        ref updated if *updated == 1 => Ok(true),
+        _ => Ok(false),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 mod config;
 mod db;
+mod errors;
 mod handlers;
 mod models;
 
-use crate::handlers::*;
+use crate::{handlers::*, models::AppState};
 use actix_web::{web, App, HttpServer};
 use dotenv::dotenv;
+use slog::{info, o, Drain, Logger};
+use slog_async;
+use slog_term;
 use std::io;
 use tokio_postgres::NoTls;
+
+fn configure_log() -> Logger {
+    let decorator = slog_term::TermDecorator::new().build();
+    let console_drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let console_drain = slog_async::Async::new(console_drain).build().fuse();
+
+    slog::Logger::root(console_drain, o!("v" => env!("CARGO_PKG_VERSION")))
+}
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
@@ -17,14 +29,18 @@ async fn main() -> io::Result<()> {
 
     let pool = config.pg.create_pool(NoTls).unwrap();
 
-    println!(
-        "Server Start http://{}:{}",
-        config.server.host, config.server.port
+    let log = configure_log();
+    info!(
+        log,
+        "Server Start http://{}:{}", config.server.host, config.server.port
     );
 
     HttpServer::new(move || {
         App::new()
-            .data(pool.clone())
+            .data(AppState {
+                pool: pool.clone(),
+                log: log.clone(),
+            })
             .route("/", web::get().to(status))
             .route("/todos{_:/?}", web::get().to(get_todos))
             .route("/todos{_:/?}", web::post().to(create_todo))
